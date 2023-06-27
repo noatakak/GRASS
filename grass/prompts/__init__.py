d2def0e23e0552d0329d5e09aaff8d39bb8b8398
Noatak Post <noatak.post@gmail.com>
2023-06-27 09:31:46 -0700

    added main w/ key to gitignore

--- grass/prompts/__init__.py
+++ grass/prompts/__init__.py
@@ -1,7 +1,7 @@
 import pkg_resources
-import voyager.utils as U
+import grass.utils as U
 
 
 def load_prompt(prompt):
-    package_path = pkg_resources.resource_filename("voyager", "")
+    package_path = pkg_resources.resource_filename("grass", "")
     return U.load_text(f"{package_path}/prompts/{prompt}.txt")
--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-from grass import grass
+from grass import Grass
 
 # You can also use mc_port instead of azure_login, but azure_login is highly recommended
 azure_login = {
     "client_id": "513c64b8-702a-42e6-9d46-21316dcaa1cf",
     "redirect_url": "https://127.0.0.1/auth-response",
     "secret_value": "rIE8Q~PQLsdM0EA_Fb8Aw.w_GFUKMHQ-CC8JBcfN",
-    "version": "fabric-loader-0.14.21-1.19.4", # the version Voyager is tested on
+    "version": "fabric-loader-0.14.21-1.19.4", # the version Grass is tested on
 }
 openai_api_key = ""
 
-grass = grass(
-    mc_port=58124,
+grass = Grass(
+    mc_port=59279,
     openai_api_key=openai_api_key,
-    #resume = True,
-    #ckpt_dir="C:/Users/noata/MinecraftVoyager/ckpt",
-    #skill_library_dir="./skill_library/trial2",
 )
 
 # start lifelong learning
 grass.learn()
-#voyager.inference(task="craft a diamond sword")
